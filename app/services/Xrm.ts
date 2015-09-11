67d1e2482e6feb506d0a9ff6aff689217cef599a
Cristhian Fernandez <crisfervil@gmail.com>
2015-09-11 18:01:59 +0100

    Added functionality to app\services\xrm

--- app/services/Xrm.ts
+++ app/services/Xrm.ts
@@ -1,13 +1,45 @@
+import {Http} from 'angular2/angular2';
+
 export class Xrm {
-  
-  Accounts = [{Name:"Account 1", Description:"Description for Account"},
-              {Name:"Account 2", Description:"Description for Account"}];
-  
-  retrieve(id, odataSetName){
-    return this.Accounts;
-  }
-  
-  retrieveMultiple (odataSetName, oDataQuery) {
+
+    private _http:Http;
+
+    constructor(http:Http, private _devURL?: string){
+        this._http = new Http;
+    };
+
+    Accounts = [{ Name: "Account 1", Description: "Description for Account" },
+        { Name: "Account 2", Description: "Description for Account" }];
+
+
+    private ODATA_ENDPOINT = "/XRMServices/2011/OrganizationData.svc";
+    private version = '2.6.0';
+
+
+    private getServerUrl() {
+        var url = "";
+        var localServerUrl = window.location.protocol + "//" + window.location.host;
+        var url = this._devURL||localServerUrl;
+        return url;
+    }
+
+    private getODataPath() {
+        return this.getServerUrl() + this.ODATA_ENDPOINT;
+    }
     
-  }  
+    retrieve(id, odataSetName) {
+        return this.Accounts;
+    }
+
+    retrieveMultiple(entityName:string, columns:Array<string>, filter:string) {
+
+        // in case filter is empty 
+        filter = (filter) ? "&$filter=" + encodeURIComponent(filter) : '';
+
+        // create defered object
+        var setName = entityName + 'Set',
+            query = this.getODataPath() + "/" + setName + "?$select=" + columns.join(',') + filter;
+            
+        return this._http.get(query);
+    }
 }
